e4481db50b0b1c2c8e3335d70313503a10c8b007
NandaScott <nanda1123@gmail.com>
2022-06-13 15:20:51 -0400

    Installed private npm package and using it

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { helloWorld } from '@nds-tickets-common/hello-world-example'
 
 function App() {
+  helloWorld()
   return (
     <div className="App">
       <header className="App-header">
